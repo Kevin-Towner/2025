210938ecb0db72b10bb2e94254c2562cd2344ceb
Kevin Towner <>
2025-04-11 09:49:56 -0400

    Save

---
+++ assets/css/aside/style.c
@@ -0,0 +1,241 @@
+.font-h1 {
+  font-family: "Outfit", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+.font-p {
+  font-family: "Raleway", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 16.5px;
+}
+aside {
+  background: #FEFEFE;
+  color: #fff;
+  z-index: 1;
+  position: sticky;
+  top: 3rem;
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.18) 0px 2px 4px;
+}
+aside p,
+aside li,
+aside span {
+  font-family: "Raleway", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 16.5px;
+}
+aside #nav-row-1 {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 20px;
+  padding: 0.5rem 0 1rem 0;
+}
+aside #nav-row-1 img {
+  width: 60px;
+  border-radius: 50%;
+  margin-left: 20px;
+}
+aside #nav-row-1 ul {
+  margin-left: 0.7rem;
+}
+aside #nav-row-1 ul #navigation-name {
+  margin-bottom: 0.3rem;
+  font-size: 18px;
+  color: #545454;
+  font-family: "Outfit", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+aside #nav-row-1 ul #navigation-role {
+  font-family: "Raleway", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  font-size: 16.5px;
+  color: #565656;
+  font-weight: 400;
+  font-size: 14.5px;
+}
+aside #nav-row-2 ul li a {
+  color: #565656;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-decoration: none;
+  padding: 2rem 2rem 2rem 0;
+  text-align: center;
+  transition: 0.3s;
+}
+aside #nav-row-2 ul li a .nav-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+aside #nav-row-2 ul li #anchor-skills .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-2 ul li #anchor-skills .nav-item p:before {
+  content: url("../../images/branding/icons/skills-default.png");
+  position: relative;
+  top: 6px;
+  padding-right: 1rem;
+}
+aside #nav-row-2 ul li #anchor-skills .nav-item p:after {
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-skills:hover {
+  background: #edeff3;
+}
+aside #nav-row-2 ul li #anchor-skills:hover p {
+  color: #0047FF;
+}
+aside #nav-row-2 ul li #anchor-skills:hover p:before {
+  content: url("../../images/branding/icons/skills-hover.png");
+}
+aside #nav-row-2 ul li #anchor-skills:hover p:after {
+  content: "⇢";
+  color: blue;
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-prof .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-2 ul li #anchor-prof .nav-item p:before {
+  content: url("../../images/branding/icons/prof-default.png");
+  position: relative;
+  top: 6px;
+  padding-right: 1rem;
+}
+aside #nav-row-2 ul li #anchor-prof .nav-item p:after {
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-prof:hover {
+  background: #edeff3;
+}
+aside #nav-row-2 ul li #anchor-prof:hover p {
+  color: #0047FF;
+}
+aside #nav-row-2 ul li #anchor-prof:hover p:before {
+  content: url("../../images/branding/icons/prof-hover.png");
+}
+aside #nav-row-2 ul li #anchor-prof:hover p:after {
+  content: "⇢";
+  color: blue;
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-projects .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-2 ul li #anchor-projects .nav-item p:before {
+  content: url("../../images/branding/icons/projects-default.png");
+  position: relative;
+  top: 6px;
+  padding-right: 1rem;
+}
+aside #nav-row-2 ul li #anchor-projects:hover {
+  background: #edeff3;
+}
+aside #nav-row-2 ul li #anchor-projects:hover p {
+  color: #0047FF;
+}
+aside #nav-row-2 ul li #anchor-projects:hover p:before {
+  content: url("../../images/branding/icons/projects-hover.png");
+}
+aside #nav-row-2 ul li #anchor-projects:hover p:after {
+  content: "⇢";
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-resume .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-2 ul li #anchor-resume .nav-item p:before {
+  content: url("../../images/branding/icons/resume-default.png");
+  position: relative;
+  top: 6px;
+  padding-right: 1rem;
+}
+aside #nav-row-2 ul li #anchor-resume:hover {
+  background: #edeff3;
+}
+aside #nav-row-2 ul li #anchor-resume:hover p {
+  color: #0047FF;
+}
+aside #nav-row-2 ul li #anchor-resume:hover p:before {
+  content: url("../../images/branding/icons/resume-hover.png");
+}
+aside #nav-row-2 ul li #anchor-resume:hover p:after {
+  content: "🔗";
+  position: relative;
+  margin-left: 1rem;
+}
+aside #nav-row-2 ul li #anchor-contact .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-2 ul li #anchor-contact .nav-item p:before {
+  content: url("../../images/branding/icons/contact-default.png");
+  position: relative;
+  top: 6px;
+  padding-right: 1rem;
+}
+aside #nav-row-2 ul li #anchor-contact:hover {
+  background: #edeff3;
+}
+aside #nav-row-2 ul li #anchor-contact:hover p {
+  color: #0047FF;
+}
+aside #nav-row-2 ul li #anchor-contact:hover p:before {
+  content: url("../../images/branding/icons/contact-hover.png");
+}
+aside #nav-row-2 ul li #anchor-contact:hover p:after {
+  content: "🔗";
+  position: relative;
+  margin-left: 2rem;
+}
+aside #nav-row-3 {
+  padding-bottom: 2rem;
+}
+aside #nav-row-3 a {
+  color: #565656;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-decoration: none;
+  padding: 1rem 0;
+  border-radius: 4px;
+  padding: 2rem 2rem 2rem 0;
+}
+aside #nav-row-3 a .nav-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+aside #nav-row-3 a .nav-item p {
+  margin-left: 2rem;
+}
+aside #nav-row-3 a .nav-item p:before {
+  content: url("../../images/branding/icons/LINKEDIN-DEFAULT.png");
+  position: relative;
+  top: 6px;
+  padding-right: 0.5rem;
+}
+aside #nav-row-3 a:hover {
+  color: purple;
+  background: #d8d8d8;
+}
+aside #nav-row-3 a:hover p {
+  color: #0047FF;
+}
+aside #nav-row-3 a:hover p:before {
+  content: url("../../images/branding/icons/LINKEDIN-HOVER.png");
+}
